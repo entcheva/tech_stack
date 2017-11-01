12a6c1f0b8addf09e6325620ac9e54897f056779
entcheva <christina.entcheva@gmail.com>
2017-11-01 13:46:47 -0400

    Add Redux, common Components

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/common';
 
 const App = () => {
   return (
-    <View />
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText='Tech 🙌 Stack' />
+      </View>
+    </Provider>
   );
 };
+
+export default App;
